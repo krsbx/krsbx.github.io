874b824ddd73875e1d0a8c8572a94fe582d02249
KRSBX <firdaus.sati50@gmail.com>
2021-01-02 10:42:36 +0700

    Fix Porfiles page
    Add navbar to profiles page

--- Scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 10)
-            $("navbar").addClass("sticky");
-        else
-            $("navbar").removeClass("sticky");
-    });
-});
