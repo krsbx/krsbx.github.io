0ca2bb088e9960e444bf58cd37881547a29dd1d6
KRSBX <firdaus.sati50@gmail.com>
2021-01-04 22:52:28 +0700

    Change Navbar Color on Scroll

--- Script/Navbar.js
+++ Script/Navbar.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+    //Changes navbar color
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass('sticky');
+        }else{
+            $('.navbar').removeClass('sticky');
+        }
+    });
+    //Menu button for responsiveness
     $('.menu-btn').click(function(){
         $(this).toggleClass("active");
         $(".navbar-menu").toggleClass("active");
